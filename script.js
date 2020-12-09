39039553aa34bf73c9e93bed8aa3db0b4ffb579f
StevenM404 <67655910+StevenM404@users.noreply.github.com>
2020-12-09 16:16:03 -0500

    attempted fix

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 character = ["!", "@", "#", "$", "%", "^", "&", "*"];
-number = [1,2,3,4,5,6,7,8,9,0];
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
@@ -11,7 +11,7 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = ps;
 
 }
 
@@ -33,8 +33,8 @@ function generatePassword() {
   if (!confirmCharacter && !confirmNumber && !confirmLowercase && !confirmUppercase) {
     alert("Requiers criteria!")
   } else if (!confirmCharacter && !confirmNumber && !confirmLowercase && !confirmUppercase) {
-    choices = character.concat(number,lowerCase,upperCase);
-  } 
+    choices = character.concat(number, lowerCase, upperCase);
+  }
   else if (!confirmCharacter && !confirmNumber && !confirmUppercase) {
     choices = character.concat(number, upperCase);
   }
@@ -56,7 +56,7 @@ function generatePassword() {
   } else if (!confirmCharacter && !confirmUppercase) {
     choices = character.concat(upperCase);
   }
-    else if (!confirmLowercase && !confirmNumber) {
+  else if (!confirmLowercase && !confirmNumber) {
     choices = lowerCase.concat(number);
 
   } else if (!confirmLowercase && !confirmUppercase) {
@@ -77,5 +77,13 @@ function generatePassword() {
   else if (!confirmUppercase) {
     choices = upperCase;
   };
-  console.log(codeLength)
+
+  for (var i = 0; i < codeLength; i++) {
+    var passwordchoices = choices[Math.floor(Math.random * choices.length)];
+    password.push(passwordchoices);
+  }
+
+  var ps = password.join("");
+    UserInput(ps);
+    return ps;
 }
